bdb32cf21645cb85f3a5cadcd33268dacafe89dd
marco.luly <marco.luly@wolterskluwer.com>
2025-02-23 21:58:33 +0100

    First version from initializr

---
+++ src/main/java/com/example/spaghetti2hex/Spaghetti2hexApplication.java
@@ -0,0 +1,13 @@
+package com.example.spaghetti2hex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spaghetti2hexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spaghetti2hexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spaghetti2hex/Spaghetti2hexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spaghetti2hex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spaghetti2hexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
