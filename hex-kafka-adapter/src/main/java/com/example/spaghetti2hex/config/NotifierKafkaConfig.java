2f368fc21df786f6934eb5268a0b9bf9fe3a48c4
marco.luly <marco.luly@wolterskluwer.com>
2025-02-24 22:18:10 +0100

    Hex ports and kafka adapter

--- hex-kafka-adapter/src/main/java/com/example/spaghetti2hex/config/NotifierKafkaConfig.java
+++ hex-kafka-adapter/src/main/java/com/example/spaghetti2hex/config/NotifierKafkaConfig.java
@@ -1,20 +1,21 @@
 package com.example.spaghetti2hex.config;
 
-import com.example.spaghetti2hex.adapters.notify.NotifierMailAdapter;
+import com.example.spaghetti2hex.adapters.notify.NotifierKafkaAdapter;
 import com.example.spaghetti2hex.ports.NotifierPort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
-public class NotifierConfig {
+public class NotifierKafkaConfig {
 
   @Autowired
-  private MailConfig mailConfig;
+  private KafkaTemplate kafkaTemplate;
 
   @Bean
   public NotifierPort notifierPort() {
-    return new NotifierMailAdapter(mailConfig.javaMailSender());
+    return new NotifierKafkaAdapter(kafkaTemplate);
   }
 
 }
